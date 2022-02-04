1867f135da4f0ad5a568fb727415807926ba7380
hanameee <hanameee@gmail.com>
2022-02-04 23:55:08 +0900

    fix: React에서 달라지는 attribute 수정

--- src/App.js
+++ src/App.js
@@ -5,38 +5,38 @@ import Form from './components/Form'
 function App() {
     return (
         <>
-            <section class="form-wrapper">
+            <section className="form-wrapper">
                 <Form />
                 <Footer />
             </section>
             <FontControlBox />
-            <dialog class="rounded-lg shadow-xl text-left">
-                <div class="w-full rounded-lg">
-                    <div class="p-6 mt-3">
-                        <h3 class="text-lg leading-6 font-medium text-gray-900">
+            <dialog className="rounded-lg shadow-xl text-left">
+                <div className="w-full rounded-lg">
+                    <div className="p-6 mt-3">
+                        <h3 className="text-lg leading-6 font-medium text-gray-900">
                             입력하신 내용을 확인해주세요.
                         </h3>
-                        <div class="text-left">
-                            <div class="mt-2">
+                        <div className="text-left">
+                            <div className="mt-2">
                                 아이디
-                                <p class="text-sm text-blue-500 bold"></p>
+                                <p className="text-sm text-blue-500 bold"></p>
                             </div>
-                            <div class="mt-2">
+                            <div className="mt-2">
                                 비밀번호
-                                <p class="text-sm text-blue-500 bold"></p>
+                                <p className="text-sm text-blue-500 bold"></p>
                             </div>
                         </div>
                     </div>
-                    <div class="bg-gray-50 px-4 py-3 flex justify-center rounded-lg">
+                    <div className="bg-gray-50 px-4 py-3 flex justify-center rounded-lg">
                         <button
                             type="button"
-                            class="bg-white text-gray-700 border border-gray-300 hover:bg-gray-50 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-gray-500 mr-2"
+                            className="bg-white text-gray-700 border border-gray-300 hover:bg-gray-50 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-gray-500 mr-2"
                         >
                             취소하기
                         </button>
                         <button
                             type="button"
-                            class="border border-transparent bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-gray-500"
+                            className="border border-transparent bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-gray-500"
                         >
                             가입하기
                         </button>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,57 +1,57 @@
 const Form = () => {
     return (
         <form
-            class="w-full max-w-md m-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-            autocomplete="off"
+            className="w-full max-w-md m-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            autoComplete="off"
         >
-            <div class="mb-4">
+            <div className="mb-4">
                 <label
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                    for="id"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="id"
                 >
                     아이디
                 </label>
                 <input
-                    class="shadow border rounded w-full py-2 px-3 text-gray-700"
+                    className="shadow border rounded w-full py-2 px-3 text-gray-700"
                     type="text"
                     placeholder="아이디를 입력해주세요."
                 />
-                <div class="mt-1 mb-3 text-xs text-red-500"></div>
+                <div className="mt-1 mb-3 text-xs text-red-500"></div>
             </div>
-            <div class="mb-4">
+            <div className="mb-4">
                 <label
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                    for="pw"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="pw"
                 >
                     비밀번호
                 </label>
                 <input
                     type="password"
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-2 leading-tight"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-2 leading-tight"
                     placeholder="비밀번호를 입력해주세요"
-                    autocomplete="off"
+                    autoComplete="off"
                 />
-                <div class="mt-1 mb-3 text-xs text-red-500"></div>
+                <div className="mt-1 mb-3 text-xs text-red-500"></div>
             </div>
-            <div class="mb-6">
+            <div className="mb-6">
                 <label
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                    for="pw-check"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="pw-check"
                 >
                     비밀번호 확인
                 </label>
                 <input
                     type="password"
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
                     placeholder="비밀번호 확인을 입력해주세요."
-                    autocomplete="off"
+                    autoComplete="off"
                 />
-                <div class="mt-1 mb-3 text-xs text-red-500"></div>
+                <div className="mt-1 mb-3 text-xs text-red-500"></div>
             </div>
-            <div class="flex items-center justify-center">
+            <div className="flex items-center justify-center">
                 <input
                     type="submit"
-                    class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-gray-500"
+                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-gray-500"
                     value="가입하기"
                 />
             </div>
