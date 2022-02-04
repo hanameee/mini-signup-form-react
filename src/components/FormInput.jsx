c8ce0d01201b32601db0e3e5902693289c5cb48a
hanameee <hanameee@gmail.com>
2022-02-05 01:35:40 +0900

    feat: 2. 유효성 검사 로직 (ID 일부 구현)

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,8 +1,21 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
+
+const ID_REGEX = new RegExp('^[a-z0-9_-]{5,20}$')
 
 const FormInput = (props) => {
     const { label, id, inputProps } = props
+    const [value, setValue] = useState('')
     const inputRef = useRef(null)
+
+    const validateInput = () => {
+        if (value.length === 0) return 'required'
+        switch (id) {
+            case 'id':
+                return ID_REGEX.test(value) ? true : 'invalidId'
+            default:
+                return
+        }
+    }
 
     useEffect(() => {
         if (id === 'id') {
@@ -22,6 +35,9 @@ const FormInput = (props) => {
                 className="shadow border rounded w-full py-2 px-3 text-gray-700"
                 id={id}
                 ref={inputRef}
+                onBlur={() => console.log(validateInput())}
+                onChange={(e) => setValue(e.target.value)}
+                value={value}
                 {...inputProps}
             />
             <div className="mt-1 mb-3 text-xs text-red-500"></div>
