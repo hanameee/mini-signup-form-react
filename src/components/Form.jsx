df11e9388a9a807836269789a7da3ce2b0485d5e
hanameee <hanameee@gmail.com>
2022-02-07 04:28:53 +0900

    fix: 유효성 검사 Form에서 하도록 수정, confirm 버튼 일부 구현

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react'
+import { useState, useContext } from 'react'
+import { FormContext } from '../App'
 import FormInput from './FormInput'
 
 const initialErrorData = {
@@ -7,56 +8,91 @@ const initialErrorData = {
     confirmPw: true,
 }
 
-export const ErrorContext = createContext({
-    errorData: initialErrorData,
-    setErrorMsg: () => {},
-})
+const ID_REGEX = new RegExp('^[a-z0-9_-]{5,20}$')
+const PW_REGEX = new RegExp('^[a-zA-Z0-9W]{8,16}$')
 
 const Form = () => {
     const [errorData, setErrorData] = useState(initialErrorData)
+    const { formData } = useContext(FormContext)
+
+    const handleOnBlur = (id) => {
+        const validateResult = validateInput(id)
+        setErrorData((errorData) => ({ ...errorData, [id]: validateResult }))
+        if (id === 'pw' && formData.confirmPw) {
+            handleOnBlur('confirmPw')
+        }
+    }
+
+    const validateInput = (id) => {
+        const value = formData[id]
+        if (value.length === 0) return 'required'
+        switch (id) {
+            case 'id':
+                return ID_REGEX.test(value) ? true : 'invalidID'
+            case 'pw':
+                return PW_REGEX.test(value) ? true : 'invalidPW'
+            case 'confirmPw':
+                return value === formData.pw ? true : 'invalidConfirm'
+            default:
+                return
+        }
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        const isValid = Object.values(errorData).every(
+            (value) => value === undefined
+        )
+        console.log(isValid, errorData)
+    }
 
     return (
-        <ErrorContext.Provider value={{ errorData, setErrorData }}>
-            <form
-                className="w-full max-w-md m-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-                autoComplete="off"
-            >
-                <FormInput
-                    label="아이디"
-                    id="id"
-                    inputProps={{
-                        type: 'text',
-                        placeholder: '아이디를 입력해주세요.',
-                        // autoFocus: true,
-                    }}
+        <form
+            className="w-full max-w-md m-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            autoComplete="off"
+            onSubmit={onSubmit}
+        >
+            <FormInput
+                label="아이디"
+                id="id"
+                inputProps={{
+                    type: 'text',
+                    placeholder: '아이디를 입력해주세요.',
+                    // autoFocus: true,
+                }}
+                handleOnBlur={handleOnBlur}
+                errorData={errorData}
+            />
+            <FormInput
+                label="비밀번호"
+                id="pw"
+                inputProps={{
+                    type: 'password',
+                    placeholder: '비밀번호를 입력해주세요',
+                    autoComplete: 'off',
+                }}
+                handleOnBlur={handleOnBlur}
+                errorData={errorData}
+            />
+            <FormInput
+                label="비밀번호 확인"
+                id="confirmPw"
+                inputProps={{
+                    type: 'password',
+                    placeholder: '비밀번호 확인을 입력해주세요.',
+                    autoComplete: 'off',
+                }}
+                handleOnBlur={handleOnBlur}
+                errorData={errorData}
+            />
+            <div className="flex items-center justify-center mt-6">
+                <input
+                    type="submit"
+                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-gray-500"
+                    value="가입하기"
                 />
-                <FormInput
-                    label="비밀번호"
-                    id="pw"
-                    inputProps={{
-                        type: 'password',
-                        placeholder: '비밀번호를 입력해주세요',
-                        autoComplete: 'off',
-                    }}
-                />
-                <FormInput
-                    label="비밀번호 확인"
-                    id="confirmPw"
-                    inputProps={{
-                        type: 'password',
-                        placeholder: '비밀번호 확인을 입력해주세요.',
-                        autoComplete: 'off',
-                    }}
-                />
-                <div className="flex items-center justify-center mt-6">
-                    <input
-                        type="submit"
-                        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-gray-500"
-                        value="가입하기"
-                    />
-                </div>
-            </form>
-        </ErrorContext.Provider>
+            </div>
+        </form>
     )
 }
 
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,9 +1,5 @@
 import { useEffect, useRef, useContext } from 'react'
 import { FormContext } from '../App'
-import { ErrorContext } from './Form'
-
-const ID_REGEX = new RegExp('^[a-z0-9_-]{5,20}$')
-const PW_REGEX = new RegExp('^[a-zA-Z0-9W]{8,16}$')
 
 const ERROR_MESSAGE = {
     invalidID:
@@ -14,37 +10,18 @@ const ERROR_MESSAGE = {
 }
 
 const FormInput = (props) => {
-    const { label, id, inputProps } = props
+    const { label, id, inputProps, handleOnBlur, errorData } = props
 
     const inputRef = useRef(null)
     const { formData, setFormData } = useContext(FormContext)
-    const { errorData, setErrorData } = useContext(ErrorContext)
-
-    const handleOnBlur = () => {
-        const validateResult = validateInput()
-        setErrorData({ ...errorData, [id]: validateResult })
-    }
-
-    const validateInput = () => {
-        const value = formData[id]
-        if (value.length === 0) return 'required'
-        switch (id) {
-            case 'id':
-                return ID_REGEX.test(value) ? true : 'invalidID'
-            case 'pw':
-                return PW_REGEX.test(value) ? true : 'invalidPW'
-            case 'confirmPw':
-                return value === formData.confirmPw ? true : 'invalidConfirmPw'
-            default:
-                return
-        }
-    }
 
     useEffect(() => {
         if (id === 'id') {
             inputRef?.current.focus()
         }
     }, [])
+
+    useEffect(() => {})
 
     return (
         <div className="mb-4">
@@ -58,17 +35,15 @@ const FormInput = (props) => {
                 className="shadow border rounded w-full py-2 px-3 text-gray-700"
                 id={id}
                 ref={inputRef}
-                onBlur={() => handleOnBlur()}
+                onBlur={() => handleOnBlur(id)}
                 onChange={(e) =>
                     setFormData({ ...formData, [id]: e.target.value })
                 }
                 {...inputProps}
             />
-            {errorData[id] !== true && (
-                <div className="mt-1 mb-3 text-xs text-red-500">
-                    {ERROR_MESSAGE[errorData[id]]}
-                </div>
-            )}
+            <div className="mt-1 mb-3 text-xs text-red-500">
+                {errorData[id] !== true && ERROR_MESSAGE[errorData[id]]}
+            </div>
         </div>
     )
 }
